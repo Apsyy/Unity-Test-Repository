2b94c4250839c25759084388519d61eafe6889e2
Anthony <apsy.hurst@gmail.com>
2022-11-24 22:29:58 +1300

    Fixed Landing SFX

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -71,6 +71,7 @@ public class PlayerScript : MonoBehaviour
             {
                 isGrounded = true;
                 hasWahooed = false;
+                landSound.Play();
                 
             }
         }
